c94cef62eb309588e94b44bdd7efa0ee7674261b
UstyuzhaninAV <UstyuzhaninAV@users.noreply.github.com>
2021-09-14 18:04:49 +0300

    some changes at variables

---
+++ secrets.go
@@ -0,0 +1,31 @@
+package main
+
+var Global = "applicationID"
+func InitApp() (string) {
+        var Global= "applicationID"
+        return Global
+}
+
+var Global = "applicationKey"
+func InitApp() (string) {
+        var Global= "applicationKey"
+        return Global
+}
+
+var Global = "bucketId"
+func InitApp() (string) {
+        var Global= "bucketId"
+        return Global
+}
+
+var Global = "nginxConfPath"
+func InitApp() (string) {
+        var Global= "nginxConfPath"
+        return Global
+}
+
+var Global = "apiUrl"
+func InitApp() (string) {
+        var Global= "apiUrl"
+        return Global
+}
